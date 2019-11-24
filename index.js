186cd91f4ef1eb8a84f2a0ceb8676aa1be412cca
thyagofernandes <thyagofernandes>
2019-11-24 19:25:10 -0300

    Finalizando os testes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const op = 1;
+var d = new Date();
 var readline = require('readline');
 const { Pool, Client } = require('pg');
 const fs = require('fs');
@@ -13,38 +14,72 @@ const pool = new Pool({
   password: 'docker',
   port: 25432,
 })
+let labels = new Map();
+labels.set('t1', 'tempo inserção ');
+labels.set('t2', 'tempo de busca restaurants a 1km');
+labels.set('t3', 'tempo de busca todos os restaurantes');
 
+const before = Date.now();
 switch (op) {
   case 1:
-   
-    var lineno = 0;
+    pool.query('delete from places'); 
     
-    myInterface.on('line', function (line) {
-      lineno++;
-      let restaurant = JSON.parse(line);
-      restaurantsArray.push(restaurant)
-      values = [restaurant.address.coord[0],restaurant.address.coord[1],restaurant.name]
-      
-      pool.query('insert into places values (ST_MakePoint($1, $2), $3);',values, (err, res) => {
-        console.log(err,res);
-      })
-    });
-
+    insertRestaurants();
     
     break;
+  case 2:
 
+    pool.query('select *, ST_Distance(lat_lng, ST_MakePoint(-73.9903, 40.7570)::geography)from places where ST_DWithin(lat_lng, ST_MakePoint(-73.98241999999999, 40.579505)::geography, 1000)order by ST_Distance(lat_lng, ST_MakePoint(-126.4, 45.32)::geography);', (err, res) => {
+      for(i = 0; i<res.rows.length;i++);
+
+      const after = Date.now();
+      console.log((after - before)/1000);
+      
+    })
+    break;
+  case 3:
+
+    pool.query('select * from places',(err,res)=>
+    {
+      for(i = 0; i<res.rows.length;i++);
+
+      const after = Date.now();
+      console.log((after - before)/1000);
+    })
   default:
     break;
 }
 
 
-console.log(restaurantsArray);
 // pools will use environment variables
 // for connection information
 
 
 
-function setrestaurants(r)
+function insertRestaurants(r)
 {
-  restaurantsArray = r;
+
+  
+    var lineno = 0;
+    var execTime = 0;
+    var variacao = 0;
+    
+      myInterface.on('line', function (line) {
+       
+        lineno++;
+        let restaurant = JSON.parse(line);
+        values = [restaurant.address.coord[0],restaurant.address.coord[1]]
+        pool.query('insert into places values (ST_MakePoint($1, $2));',values, (err, res) => {
+          
+          const after = Date.now();
+          console.log((after - before)/1000);
+          
+        })
+       
+        
+    });
+    
+    
+    
+  
 }
